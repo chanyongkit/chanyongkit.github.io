e4fe29dc1c73e2e45c1f43cfe52d337973d29fa2
chanyongkit <salsacyk@gmail.com>
2021-06-11 00:06:29 +0800

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <v-app>
+    <v-container fill-height fluid>
+      <v-row justify="center" align="center">
+        <vue-typed-js :strings="['Work in progress...','Stay tuned ;)']">
+        <h1 class="typing"></h1>
+        </vue-typed-js>
+      </v-row>
+    </v-container>
+  </v-app>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueTypedJs from 'vue-typed-js'
+Vue.use(VueTypedJs)
+
+export default {
+  components: {
+  }
+}
+</script>
